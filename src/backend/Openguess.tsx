64759c48ce8eef41746f2c293de3e6b8b864720f
Kamelowy <kamilcz2004@gmail.com>
2022-10-02 21:31:07 +0200

    Delete src/backend directory

--- src/backend/Openguess.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useState } from 'react'
-import '../pages/Guess.css'
-import { Loader } from '@googlemaps/js-api-loader'
-
-export default function Openguess() {
-  return(
-    <div>Openguess</div>
-  ) 
-}
